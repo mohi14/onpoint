34cdf1ed18c95bde38bf8e1a94c0e6918093f105
mohi14 <mohiuddinkhan1409@gmail.com>
2023-02-15 23:58:04 +0600

    nav item animated

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,31 +1,32 @@
 import React from 'react';
+import './Navbar.css'
 
 const Navbar = () => {
     return (
         <div className='container mx-auto flex justify-between items-center py-[44px]'>
-            <div>
+            <div className='cursor-pointer'>
                 <img src={require("../Assets/logo/logo.png")} alt="" />
             </div>
 
-            <div className='flex text-[#FFFFFF] font-[500] text-[20px] gap-[55px]'>
-                <div className='hover:text-[#CEFF45]'>
+            <div className='hidden lg:flex text-[#FFFFFF] font-[500] text-[20px] gap-[55px]'>
+                <div className='  navitem'>
                     <a href="">Home</a>
                 </div>
-                <div className='hover:text-[#CEFF45]'>
+                <div className=' navitem'>
                     <a href="">Portfolios</a>
                 </div>
-                <div className='hover:text-[#CEFF45]'>
+                <div className=' navitem'>
                     <a href="">Services</a>
                 </div>
-                <div className='hover:text-[#CEFF45]'>
+                <div className=' navitem'>
                     <a href="">Work</a>
                 </div>
-                <div className='hover:text-[#CEFF45]'>
+                <div className=' navitem'>
                     <a href="">Testimonials</a>
                 </div>
             </div>
 
-            <div className='text-black w-[214px] h-[67px] bg-[#CEFF45] rounded-[5px] flex justify-center items-center font-[500] text-[18px]'>
+            <div className='text-black w-[214px] h-[67px] bg-[#CEFF45] start-button  rounded-[5px] flex justify-center items-center font-[500] text-[18px] cursor-pointer'>
                 <button>Start a project?</button>
             </div>
         </div>
